f0102dfc06843c248644df1b5809414465c6a59c
johnziegenbein <johnziegenbein1993@gmail.com>
2021-10-09 13:06:50 +0200

    Do nothing on turn 1 start
    Since no combat has been fought previously we do no need to store
    information about combat here.

--- HdtEventManager.cs
+++ HdtEventManager.cs
@@ -1,4 +1,5 @@
-﻿using Hearthstone_Deck_Tracker.Utility.Logging;
+﻿using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.Utility.Logging;
 
 namespace bgs_never_lucky
 {
@@ -7,19 +8,20 @@ namespace bgs_never_lucky
         public void OnGameStart()
         {
             Log.Info("Never lucky: OnGameStart called");
-            if(!Hearthstone_Deck_Tracker.Core.Game.IsBattlegroundsMatch) return;
+            if(!Core.Game.IsBattlegroundsMatch) return;
         }
 
         public void OnGameEnd()
         {
             Log.Error("Never lucky: OnGameEnd called");
-            if(!Hearthstone_Deck_Tracker.Core.Game.IsBattlegroundsMatch) return;
+            if(!Core.Game.IsBattlegroundsMatch) return;
         }
 
         public void OnTurnStart()
         {
             Log.Info("Never lucky: OnTurnStart called");
-            if(!Hearthstone_Deck_Tracker.Core.Game.IsBattlegroundsMatch) return;
+            if(!Core.Game.IsBattlegroundsMatch) return;
+            if(Core.Game.GetTurnNumber() == 1) return;
         }
     }
 }
