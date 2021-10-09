41a16e33e35fe8d627448be662841f72c620b72a
johnziegenbein <johnziegenbein1993@gmail.com>
2021-10-09 12:55:10 +0200

    Validate game mode
    The plugin is only meant to be used in BG mode.

--- HdtEventManager.cs
+++ HdtEventManager.cs
@@ -7,16 +7,19 @@ namespace bgs_never_lucky
         public void OnGameStart()
         {
             Log.Info("Never lucky: OnGameStart called");
+            if(!Hearthstone_Deck_Tracker.Core.Game.IsBattlegroundsMatch) return;
         }
 
         public void OnGameEnd()
         {
             Log.Error("Never lucky: OnGameEnd called");
+            if(!Hearthstone_Deck_Tracker.Core.Game.IsBattlegroundsMatch) return;
         }
 
         public void OnTurnStart()
         {
             Log.Info("Never lucky: OnTurnStart called");
+            if(!Hearthstone_Deck_Tracker.Core.Game.IsBattlegroundsMatch) return;
         }
     }
 }
